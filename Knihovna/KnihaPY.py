8a826097b860c07c96c185f096545c43c5974387
Antonín Mužák <muzakton@gmail.com>
2025-01-07 17:28:58 +0100

    Update KnihaPY.py

--- Knihovna/KnihaPY.py
+++ Knihovna/KnihaPY.py
@@ -1,13 +1,11 @@
-
-
-class Kniha:
+Class Kniha:
     def __init__(self, nazev:str, autor:str, vydani:int) -> None:
         self.nazev:str = nazev
         self.autor:str = autor
         self.rok_vydani:int = vydani
         self.je_vypujcena:bool = False
         
-    #toString
+    
     def __str__(self) -> str:
         return f"[{self.nazev}] ({self.autor} - {self.rok_vydani})"
     
